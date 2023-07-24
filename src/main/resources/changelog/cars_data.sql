c2c4817a884673c5a58bc9cf3d17d3b05eaff63b
GromGT12 <m.krymov@wp.pl>
2023-07-24 20:48:58 +0200

    home_work добавил скрипты для создания и заполнения БД

---
+++ src/main/resources/changelog/cars_data.sql
@@ -0,0 +1,5 @@
+ INSERT INTO public.cars(
+     id, brand, model, class, fuel, price_per_da, parking_id)
+    VALUES ( 1, 'porsche', 'panamera', 'business', 'petrol', 190, 30),
+    ( 2, 'lexus', 'es300h', 'business', 'hybrid', 170, 31),
+    ( 3, 'jaguar', 'xf', 'business', 'petrol', 180, 32);
---
+++ src/main/resources/changelog/parkings_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.parkings(
+     id, name, phone)
+	VALUES ( 30, 'underground parking', '786987123'),
+	( 31, 'ground parking', '345678034'),
+	( 32, 'multi-level parking', '849780066');
