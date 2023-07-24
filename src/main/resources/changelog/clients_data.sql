ef8a84a61b8ff5ee4c2d37a6f70180090808a9ec
GromGT12 <m.krymov@wp.pl>
2023-07-24 20:48:58 +0200

    home_work добавил скрипты для создания и заполнения БД

---
+++ src/main/resources/changelog/clients_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.clients(
+	 id, first_name, last_name, document_number, accidents)
+	VALUES ( 10, 'mark', 'weber', 'fxd4567890', 0),
+	( 11, 'rodrigez', 'garcia', 'czx3586239', 2),
+	( 12, 'giuseppe', 'rossi', 'tix9512095', 3);
