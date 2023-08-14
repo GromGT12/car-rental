f972d6e3bdb1732a66c05d7a59ece8fd89cbaa96
GromGT12 <m.krymov@wp.pl>
2023-08-14 18:58:30 +0200

    Добавил последний эндпоинт, Hibernate security. И Swagger корректно работает

--- src/main/java/pl/maks/carrental/security/SecurityConfig.java
+++ src/main/java/pl/maks/carrental/security/SecurityConfig.java
@@ -4,25 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder()
-                        .username("calculate-Rental-Price")
-                        .password("password")
-                        .roles("USER")
-                        .build(),
-                User.withDefaultPasswordEncoder()
-                        .username("admin")
-                        .password("password")
-                        .roles("ADMIN")
-                        .build()
-        );
+    public InMemoryUserDetailsManager users() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("client")
+                .password("password")
+                .roles("CLIENT")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("chacha")
+                .roles("ADMIN")
+                .authorities("ADMIN")
+                .build();
+        return  new InMemoryUserDetailsManager(user, admin);
+
     }
 }
