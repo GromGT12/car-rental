4c0090f59a06e144efc7a888fe2c2859829c5a0c
GromGT12 <m.krymov@wp.pl>
2023-08-14 19:11:23 +0200

    Добавил последний эндпоинт, Hibernate security. И Swagger корректно работает

--- src/main/java/pl/maks/carrental/security/SecurityConfig.java
+++ src/main/java/pl/maks/carrental/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package pl.maks.carrental.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +29,23 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .authorities("ADMIN")
                 .build();
-        return  new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.GET, "/clients/**", "/contracts/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/clients", "/contracts/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/clients/**", "/contracts/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/clients/**").hasRole("ADMIN")
+                        .requestMatchers("/hello/**").hasRole("HELLO")
+                        .requestMatchers("/random-joke").permitAll()
+                        .anyRequest().authenticated())
+                .build();
     }
 }
+
